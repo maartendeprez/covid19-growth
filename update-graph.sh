ae47c3bb4d775226b88204b354f01fb25919afbd
Maarten Deprez <deprez.maarten@gmail.com>
2020-03-28 12:03:09 +0100

    Add package.yaml and move generated graphs to graph/ folder

--- update-graph.sh
+++ update-graph.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# regions =( China South-Korea Italy Spain Belgium Netherlands Brazil Switzerland France Germany US UK )
-regions=( China South-Korea Italy Belgium Brazil US )
+regions=( China South-Korea Italy Spain Belgium Netherlands Brazil Switzerland France Germany US UK )
+#regions=( China South-Korea Italy Belgium Brazil US )
 
-stack run -- --source worldometers --minimum 10 --smoothing 7 --graph active "${regions[@]}" > active.html
-stack run -- --source worldometers --minimum 10 --smoothing 7 --graph confirmed "${regions[@]}" > confirmed.html
-stack run -- --source worldometers --minimum 2 --smoothing 7 --graph deaths "${regions[@]}" > deaths.html
-stack run -- --source worldometers --minimum 10 --smoothing 7 --avg-smoothing 3 --graph --daily confirmed "${regions[@]}" > confirmed-daily.html
-stack run -- --source worldometers --minimum 2 --smoothing 7 --avg-smoothing 3 --graph --daily deaths "${regions[@]}" > deaths-daily.html
+stack run -- --source worldometers --minimum 10 --smoothing 7 --graph active "${regions[@]}" > graphs/active.html
+stack run -- --source worldometers --minimum 10 --smoothing 7 --graph confirmed "${regions[@]}" > graphs/confirmed.html
+stack run -- --source worldometers --minimum 2 --smoothing 7 --graph deaths "${regions[@]}" > graphs/deaths.html
+stack run -- --source worldometers --minimum 10 --smoothing 7 --avg-smoothing 3 --graph --daily confirmed "${regions[@]}" > graphs/confirmed-daily.html
+stack run -- --source worldometers --minimum 2 --smoothing 7 --avg-smoothing 3 --graph --daily deaths "${regions[@]}" > graphs/deaths-daily.html
 
-scp *.html crissaegrim.be.eu.org:covid19/
+scp graphs/*.html crissaegrim.be.eu.org:covid19/
